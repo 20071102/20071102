fb4ba5392900adf54af3a0e0a3ce768f45e48dca
20071102 <142422447+20071102@users.noreply.github.com>
2023-08-18 01:36:29 +0900

    index-firebase-jp.php

---
+++ index-firebase-jp.php
@@ -0,0 +1,12 @@
+//TODO: REplace with your project's customized code snippet
+<script src="https://firebase.google.com/docs/web/setup#available-libraries"></script>
+<script>
+//Initia Firebase
+var config = {
+apikey: '<AIzaSyBzA891lSOIr1W3D6vRAxRdP8cOseg5QU8>'
+authDomain: '<evenpromax.firebaseapp.com>'
+databaseURL: '<https://evenpromax-default-rtdb.firebaseio.com/>'
+storageBucket: '<evenpromax.appspot.com>'
+};
+firebase.initializeApp(config);
+</script>
