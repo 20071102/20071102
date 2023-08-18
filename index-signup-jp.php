ef615c78d0b8b42d97b69fe6c04b78e37ce727db
20071102 <142422447+20071102@users.noreply.github.com>
2023-08-18 17:28:42 +0900

    index-signup-jp.php

---
+++ index-signup-jp.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>アカウント作成</title>
+</head>
+<body>
+<form method="post" action="test.html">
+<p>名前</p>
+</body>
+<input type="text" maxlength="10" placeholder="山田太郎">
+<p>ユーザーID</p>
+<input type="text" maxlength="25" placeholder="Taro123456">
+<p>メールアドレス</p>
+<input type="text" maxlength="50" placeholder="clr@example.com">
+<p>パスワード</p>
+<input type="password" maxlength="64" placeholder="Taro123456">
+<p>生年月日</p>
+<input type="date" name="calender" min="1900-01-01" max="2099-12-31">
+<p><input type="submit" value="アカウント作成を行う"></p>
+</form>
+</body>
+</html>
