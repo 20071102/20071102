25abcb3a3ddae0ae2eb3bf7d72cdddd46b074861
20071102 <142422447+20071102@users.noreply.github.com>
2023-08-27 06:44:11 +0900

    index-0002.php

--- index-0002.php
+++ index-0002.php
@@ -1,3 +1,8 @@
 blank_issues_enabled: true
 contact_links:
- -name:
+ -name: Bitccoin Core Security Policcy
+  url: https://github.com/20071102/20071102/blob/master/SECURITY.md
+  about: View security policy
+ -name: Bitcoin Core Developers
+  url: https://bitcoincore.org
+  about: Bit Core homepage
