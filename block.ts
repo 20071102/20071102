ebe232f132ab2c5bdeded3b6a285a2fba097ae8e
20071102 <142422447+20071102@users.noreply.github.com>
2023-09-02 06:06:36 +0900

    block.ts

---
+++ block.ts
@@ -0,0 +1,14 @@
+type Block = {
+  //何番目のブロックか
+  index: number;
+  //ブロックを作った時間
+  time: string;
+  //ひとつ前のブロックのハッシュ
+  prev_hash: string;
+  //このブロックのハッシュ
+  hash: string;
+  //トランザクション
+  tx: Tx;
+  //このブロックを保証してくれるバリデーター
+  validator: Validator;
+};
